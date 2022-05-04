e2f3816c5df4dc98b89ad31d068c447718eaf998
Martin Dickie <mbdickie@umass.edu>
2022-05-04 03:21:41 -0400

    changed to kasa

--- read_arduino.py
+++ read_arduino.py
@@ -1,81 +1,87 @@
 import serial
+from kasa import SmartDevice, SmartStrip, SmartBulb, SmartPlug, SmartLightStrip
+import asyncio
 import time
-import avs
 
-
-# alexa = avs.new()
+arduino = object
 off_state = {}
 empty_timestamp = time.time()
-arduino = {}
-rooms = {}
-out_time = 10
+devices = []  # KASA smart devices
+out_time = 10  # seconds
 
-def turnoff(room):
-    if room in rooms:
-        outlet = rooms[room]
+def turnoff(device):
+    if device.is_strip():
+        for plug in device.children():
+            if "light" in plug.alias:
+                plug.turn_off()
+    elif device.is_plug() or device.is_bulb() or device.is_light_strip():
+        device.turn_off()
     else:
-        raise Exception("Room not found")
-    outlet_control(outlet)
+        raise Exception("Device unknown")
+    device.update()
 
-def turnon(room):
-    if room in rooms:
-        outlet = rooms[room]
+def turnon(device):
+    if device.is_strip():
+        for plug in device.children():
+            if "light" in plug.alias:
+                plug.turn_on()
+    elif device.is_plug() or device.is_bulb() or device.is_light_strip():
+        device.turn_on()
     else:
-        raise Exception("Room not found")
-    outlet_control(outlet)
+        raise Exception("Device unknown")
+    device.update()
+
+def load_devices_from_file()
+    
+    with open("devices.txt", "r") as device_file:
+        device_addrs = device_file.read()
+        device_addr_list = device_addrs.split("\n")
+        for device_addr in device_addr_list:
+            if (!device_addr[0] == "#"):
+                devices.append(SmartDevice(device_addr))
+
+def scan_for_devices():
+    pass
+
+def load_devices():
+    if "devices.txt" in os.listdir():
+        load_devices_from_file()
+    if devices == []:
+        scan_for_devices()
+    for device in devices:
+        if device.is_strip():
+            devices.remove(device)
+            for plug in device.children():
+                devices.append(plug)
+
+def load_arduino():
+    arduino = serial.Serial(f'/dev/ttyACA0')
 
 
-def outlet_control(outlet):
-    print(outlet)
-    # TODO implement alexa control of outlet
-"""def outlet_control(outlet):
-    if type(outlet) == str:
-        alexa.lambda({
-            alexa.toggle(outlet)
-            })
-    elif type(outlet) == list:
-        for plug in outlet:
-            alexa.lambda({
-                alexa.toggle(plug)
-                })
-    else:
-        raise Exception("invalid outlet input")
-"""
+def loop():
+    for device in devices:
+        ser_bytes = arduino.readline().decode('utf-8')
+        print(ser_bytes)
+        if(ser_bytes == "occupied"):  # if the room is occupied,
+            if(off_state == 1):  # check if it is turned off, if so turn on.
+                turnon(device)
+                off_state[device] = 0
+        if(ser_bytes == "empty"):  # if the room is empty record time
+            empty_timestamp = time.time()
+        current_time = time.time()
+        # if it has been 30 minutes and is in the offstate, turn off and set the state.
+        if (off_state == 1 and (current_time - out_time > empty_timestamp)):
+            turnoff(device)
+            off_state[device] = 1
+    time.sleep(1)  # sleep 1 second
 
-def load_rooms():
-    room_data = {}
-    with open("rooms.txt", "r") as room_file:
-        room_file_data = room_file.read()
-        room_list = room_file_data.split("\n")
-        for room in room_list:
-            if room[0] == "#":
-                break
-            room_split = room.split(" ")
-            room_data[room_split[0]] = [x for x in room_split[1:]]
-    return room_data
 
-def load_arduino(rooms):
-    for room in enumerate(rooms):
-        arduino[room[1]] = serial.Serial(f'/dev/ttyACM{room[0]}')
-
+def __init__():
+    load_devices()
+    load_arduino()
 
 if __name__ == "__main__":
-    rooms = load_rooms()
+    __init__()
     while True:
-        for room in rooms:
-            ser_bytes = arduino[room].readline().decode('utf-8')
-            print(ser_bytes)
-            if(ser_bytes == "occupied"):  # if the room is occupied,
-                if(off_state == 1):  # check if it is turned off, if so turn on.
-                    turnon(room)
-                    off_state[room] = 0
-            if(ser_bytes == "empty"):  # if the room is empty record time
-                empty_timestamp = time.time()
-            current_time = time.time()
-            # if it has been 30 minutes and is in the offstate, turn off and set the state.
-            if (off_state == 1 and (current_time - out_time > empty_timestamp)):
-                turnoff(room)
-                off_state[room] = 1
-        time.sleep(1)  # sleep 1 second
-
+        loop()
 
